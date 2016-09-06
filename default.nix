8e232ce77ae04421a99be6787f1d185e49ebbc63
Scott Olson <scott@solson.me>
2016-09-05 22:56:01 -0600

    Update for changes in buildGoPackage.
    goPackages was removed. Instead, we use a deps.json file (which can be generated
    with `go2nix`)..

--- default.nix
+++ default.nix
@@ -1,18 +1,7 @@
-{ goPackages, fetchFromGitHub, lib, ... }: with goPackages;
+{ buildGoPackage, fetchFromGitHub, lib, ... }:
 (buildGoPackage rec {
   name = "systemd-digitalocean-generator";
   goPackagePath = name;
   src = builtins.filterSource (path: type: lib.hasSuffix ".go" path) ./.;
-  buildInputs = let
-    netlink = buildGoPackage rec {
-      owner  = "vishvananda";
-      repo   = "netlink";
-      rev    = "5a5eb317d73bc513ae0cfea7d9ac3c39f145e1db";
-      sha256 = "171pv8cmw8iwm5ci7hdxrkxf0bj34hihr0q950jvilwyh8sasck6";
-
-      name = repo;
-      goPackagePath = "github.com/${owner}/${repo}";
-      src = fetchFromGitHub { inherit owner repo rev sha256; };
-    };
-  in [ go-systemd netlink ];
+  goDeps = ./deps.json;
 }).bin
