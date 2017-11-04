04197df21f506b51db50fc97ecf6999f6c94cdf0
Dmitry Moskowski <me@corpix.ru>
2017-11-04 23:58:02 +0000

    Converted json file into nix
    To fix an error:
    syntax error, unexpected ':', expecting '.' or '=', at <REDACTED>/systemd-digitalocean/deps.json:3:20

---
+++ deps.nix
@@ -0,0 +1,20 @@
+[
+  {
+    goPackagePath = "github.com/coreos/go-systemd";
+    fetch = {
+      type = "git";
+      url = "https://github.com/coreos/go-systemd";
+      rev = "a606a1e936df81b70d85448221c7b1c6d8a74ef1";
+      sha256 = "0fhan564swp982dnzzspb6jzfdl453489c0qavh65g3shy5x8x28";
+    };
+  }
+  {
+    goPackagePath = "github.com/vishvananda/netlink";
+    fetch = {
+      type = "git";
+      url =  "https://github.com/vishvananda/netlink";
+      rev = "5a5eb317d73bc513ae0cfea7d9ac3c39f145e1db";
+      sha256 = "171pv8cmw8iwm5ci7hdxrkxf0bj34hihr0q950jvilwyh8sasck6";
+    };
+  }
+]
